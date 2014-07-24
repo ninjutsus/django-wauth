5d9264976d48b73405552ebe90033eac0a98e154
wilson <wiljm0.0@gmail.com>
2014-07-24 14:14:00 -0500

    adding modal for extends and comment demo style css

--- wauth/urls.py
+++ wauth/urls.py
@@ -2,10 +2,12 @@ __author__ = 'wils0n'
 
 from django.conf.urls import patterns, include, url
 from .views import *
+from django.views.generic import *
 
 urlpatterns = patterns('',
                        url(r'^$', BaseView.as_view(), name='auth_login'),
                        url(r'^auth/login/external/$', LoginExternalView.as_view(), name='auth_login_external'),
                        url(r'^auth/logout/$', 'wauth.views.logout_view', name='auth_logout'),
                        url(r'^auth/login/$', 'wauth.views.login_view', name='auth_login'),
+                       url(r'^sandbox/test/$', TemplateView.as_view(template_name='wauth/test.html')),
                        )
